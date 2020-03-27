96f27baaaeb3e6e006342f2eaf3b6b7555e4b89e
Olha <heshtegolechka@gmail.com>
2020-03-27 13:23:22 +0200

    сделала 2 задачи из книжки по питону и брат должен скачать sims4 :)

---
+++ python2.2.py
@@ -0,0 +1,10 @@
+import pickle
+favourite = ['sims4','nfs','food','films']
+f = open('favorites.dat','wb')
+pickle.dump(favourite , f)
+f.close()
+
+import pickle
+f = open('favorites.dat','rb')
+favourite = pickle.load(f)
+print(favourite)
